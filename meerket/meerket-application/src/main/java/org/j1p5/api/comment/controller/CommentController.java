bbf617e51c9916b3de46587b94d038261700f5c2
sunghyun <20190171@inu.ac.kr>
2024-12-03 00:18:48 +0900

    feat : Comment 조회 컨트롤러 구현

--- meerket/meerket-application/src/main/java/org/j1p5/api/comment/controller/CommentController.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/comment/controller/CommentController.java
@@ -3,10 +3,17 @@ package org.j1p5.api.comment.controller;
 import lombok.RequiredArgsConstructor;
 
 import org.j1p5.api.comment.dto.request.CommentCreateRequestDto;
+import org.j1p5.api.comment.dto.response.CommentReadResponseDto;
 import org.j1p5.api.comment.usecase.CommentCreateUsecase;
+import org.j1p5.api.comment.usecase.CommentReadUsecase;
 import org.j1p5.api.global.annotation.LoginUser;
 import org.j1p5.api.global.response.Response;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
 
     private final CommentCreateUsecase commentCreateUsecase;
+    private final CommentReadUsecase commentReadUsecase;
 
     @PostMapping("/{productId}")
     public Response<Void> createComment(@PathVariable(name = "productId") Long productId,
@@ -22,5 +30,15 @@ public class CommentController {
 
         commentCreateUsecase.createComment(productId,userId,request);
         return Response.onSuccess();
+    }
+
+    @GetMapping("/{productId}")
+    public Response<List<CommentReadResponseDto>> getComment(@PathVariable(name = "productId") Long productId,
+                                                            @LoginUser Long userId,
+                                                            @RequestParam(name = "page",defaultValue = "0") int page,
+                                                            @RequestParam(name = "size", defaultValue = "10") int size){
+
+        Pageable pageable = PageRequest.of(page,size);
+        return Response.onSuccess(commentReadUsecase.getAllComments(productId,userId,pageable));
     }
 }
