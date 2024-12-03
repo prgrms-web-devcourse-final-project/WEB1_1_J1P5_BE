ec993e8c753afe4813b6d518b62123a7b9328b02
yechan <rladPcks980@gmail.com>
2024-12-03 18:47:50 +0900

    docs : 스웨거설정 추가

--- meerket/meerket-application/src/main/java/org/j1p5/api/chat/controller/ChatController.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/chat/controller/ChatController.java
@@ -1,5 +1,9 @@
 package org.j1p5.api.chat.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.j1p5.api.chat.dto.ChatRoomType;
 import org.j1p5.api.chat.dto.request.ChatMessageRequest;
@@ -20,6 +24,7 @@ import java.util.List;
 /**
  * @author yechan
  */
+@Tag(name = "chats", description = "채팅 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/chats")
@@ -33,13 +38,15 @@ public class ChatController {
     private final GetUserChatRoomsUseCase userChatRoomsUseCase;
 
 
-    /**
-     * productId를 받아 낙찰자와 판매자간의 채팅방 생성
-     *
-     * @param productId 상품id
-     * @return 생성된 roomId
-     */
     @PostMapping("/{productId}")
+    @Operation(summary = "채팅방 생성", description = "낙찰자와 판매자 간의 채팅 생성")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "채팅방 생성 성공"),
+                    @ApiResponse(responseCode = "404", description = "PRODUCT404 상품 찾기 실패"),
+                    @ApiResponse(responseCode = "404", description = "CHAT_RECEIVER_404 상대 찾기 실패")
+            }
+    )
     public Response<CreateChatRoomResponse> createChatRoom(
             @PathVariable Long productId
 //            @LoginUser Long userId
@@ -52,6 +59,15 @@ public class ChatController {
 
 
     @PostMapping("/enter/{roomId}")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "채팅방 입장 성공"),
+                    @ApiResponse(responseCode = "400", description = "ROOM_ID_400 roomId 올바르지 않음"),
+                    @ApiResponse(responseCode = "404", description = "CHAT_ROOM_404 채팅방을 찾을 수 없음"),
+                    @ApiResponse(responseCode = "500", description = "CHAT_ROOM_500 채팅방 업데이트 실패(안읽은 메시지 기록)")
+            }
+    )
+    @Operation(summary = "채팅방 입장", description = "특정 채팅방에 입장시 채팅방 정보와 최근 메시지30개를 반환합니다.")
     public Response<ChatRoomEnterResponse> enterChatRoom(
             @PathVariable String roomId
 //            @LoginUser Long userId
@@ -64,6 +80,15 @@ public class ChatController {
 
 
     @PostMapping("/exit/{roomId}")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "채팅방 나가기 성공"),
+                    @ApiResponse(responseCode = "400", description = "ROOM_ID_400 roomId 올바르지 않음"),
+                    @ApiResponse(responseCode = "403", description = "CHAT_ROOM_403 채팅방에 속해있지 않은 유저"),
+                    @ApiResponse(responseCode = "404", description = "CHAT_ROOM_404 채팅방을 찾을 수 없음")
+            }
+    )
+    @Operation(summary = "채팅방 나가기", description = "특정 채팅방에서 나갑니다. 상대방에게 더 이상 채팅을 받지 못합니다.")
     public Response<Void> exitChatRoom(
             @PathVariable String roomId
 //            @LoginUser Long userId
@@ -77,6 +102,17 @@ public class ChatController {
 
 
     @GetMapping("/messages")
+    @Operation(summary = "메시지 받아오기",
+            description = "특정 날짜 이전의 메시지를 30개 조회합니다 beforeTime값이 null일때는 최근메시지를 기준으로 조회합니다..")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "메시지 조회 성공"),
+                    @ApiResponse(responseCode = "400", description = "ROOM_ID_400 roomId 올바르지 않음"),
+                    @ApiResponse(responseCode = "403", description = "CHAT_ROOM_403 채팅방에 속해있지 않은 유저"),
+                    @ApiResponse(responseCode = "404", description = "CHAT_ROOM_404 채팅방을 찾을 수 없음"),
+                    @ApiResponse(responseCode = "500", description = "CHAT_READ_500 메시지를 불러오던 중 에러 발생")
+            }
+    )
     public Response<List<ChatMessageResponse>> getChatMessages(
             @RequestParam String roomId,
             @RequestParam(required = false) LocalDateTime beforeTime
@@ -91,6 +127,17 @@ public class ChatController {
 
 
     @MessageMapping("/messages")
+    @Operation(summary = "메시지 보내기", description = "특정 채팅방에 메시지를 보냅니다.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "메시지 조회 성공"),
+                    @ApiResponse(responseCode = "400", description = "ROOM_ID_400 roomId 올바르지 않음"),
+                    @ApiResponse(responseCode = "403", description = "CHAT_ROOM_403 채팅방에 속해있지 않은 유저"),
+                    @ApiResponse(responseCode = "404", description = "CHAT_ROOM_404 채팅방을 찾을 수 없음"),
+                    @ApiResponse(responseCode = "500", description = "CHAT_SAVE_500 메시지를 저장 중 에러 발생"),
+                    @ApiResponse(responseCode = "500", description = "CHAT_RECEIVER_FIND_500 상대의 접속 여부를 조회중 에러 발생"),
+            }
+    )
     public Response<Void> sendChatMessage(
             @RequestBody @Validated ChatMessageRequest request
 //            @LoginUser Long userId,
@@ -105,6 +152,14 @@ public class ChatController {
 
 
     @GetMapping
+    @Operation(summary = "채팅방 목록 조회", description = "채팅방 목록을 조회합니다.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "채팅방 목록 조회 성공"),
+                    @ApiResponse(responseCode = "400", description = "CHAT_ROOM_400 채팅방 목록 조회 타입 에러"),
+                    @ApiResponse(responseCode = "500", description = "CHAT_ROOM_500 채팅방 목록 조회 중 서버에러 "),
+            }
+    )
     public Response<List<ChatRoomInfoResponse>> getUserChatRooms(
 //            @LoginUser Long userId,
             @RequestParam ChatRoomType type
